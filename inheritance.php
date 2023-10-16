b4559dd2bdcf1349247e378c76b50112319ded2f
miftt <miftahfauzi012@gmail.com>
2023-10-16 21:34:11 +0700

    Menambahkan inheritance #2 dan overriding

---
+++ inheritance.php
@@ -0,0 +1,78 @@
+<?php
+
+// jualan produk
+// komik
+// game
+
+class Produk
+{
+    public $judul,
+        $penulis,
+        $penerbit,
+        $harga,
+        $jmlHalaman,
+        $waktuMain;
+
+    public function __construct(
+        $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = 0,
+        $jmlHalaman = 0,
+        $waktuMain = 0,
+    ) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+    }
+
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "{$this->judul} | {$this->getLabel()} | (Rp. {$this->harga}) ";
+        return $str;
+    }
+}
+
+class Komik extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str = "Komik: {$this->judul} | {$this->getLabel()} | (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str = "Game: {$this->judul} | {$this->getLabel()} | (Rp. {$this->harga}) ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+class CetakInfoProduk
+{
+    //type objectnya adalah produk
+    public function cetak(Produk $produk)
+    {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 = new Komik("Naruto", "musashi", "moontoon", 250000, 100, 0,);
+$produk4 = new Game("Uncharted", "Moontoon", "Playstation", 250000, 0, 50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk4->getInfoProduk();
